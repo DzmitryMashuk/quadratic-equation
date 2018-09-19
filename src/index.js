48f19af003d8b6d90b1d74747b10140736f13cb1
DzmitryMashuk <d.mashuk@itransition.com>
2018-09-19 11:40:08 +0300

    Adding a solveEquation method.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let a = equation.match("[-]?\\d+ \\* x\\^2")[0];
+  let b = equation.match("[+-] \\d+ \\* x")[0];
+  let c = equation.match("[+-] \\d+$")[0];
+
+  a = a.split(' ').join('').match("[-]?\\d+");
+  b = b.split(' ').join('').match("[+-]\\d+");
+  c = c.split(' ').join('').match("[+-]\\d+");
+
+  let d  = Math.pow(b, 2) - 4 * a * c;
+  let x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+  let x2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+
+  return [x1, x2].sort(function(a, b) {
+  	return a - b;
+  });
 }
